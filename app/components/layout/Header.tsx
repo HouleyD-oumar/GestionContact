5b87c4dd24a73e942afabf9361fa3234822d7b9e
HouleyD-oumar <oumar.pro_sch@outlook.fr>
2025-09-26 22:05:59 +0200

    setup de base

---
+++ app/components/layout/Header.tsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <div className="font-sans flex justify-center align-items-center  px-3.5 fixed top-0 w-full">
+        <h1 className="text-4xl font-bold">Gestion Contact</h1>
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+import Form from "./components/Form";
+import ListGrid from "./components/ListGrid";
+import Header from "./components/layout/Header";
+export default function Home() {
+  return (
+    <div className="W-full h-screen  grid grid-rows[80px_1fr_3fr] items-center justify-center">
+        <Header />
+       <Form />
+      <ListGrid />
+      {/* FOOTER à implementer */}
+    </div>
+  );
+}
